2ff9e8891871a277d8134fce304aaa11255184d6
Nico <nicolas.albornoz996@gmail.com>
2023-01-10 04:09:39 -0300

    8

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
-app.use ('/productos', productRoutes);
-
-
+app.use ('/', productRoutes);
 app.use ('/clientes', clientesRoutes);
 
 
